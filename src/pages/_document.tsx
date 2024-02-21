eae21d2b9f6dc0e13d993d3fd36bfed0cc62bfd1
LukasLimalkl <lukinhaslima063@gmail.com>
2024-02-21 13:09:56 -0300

    feat: configurando styled-components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  public static async getInitialProps(ctx: DocumentContext ) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  public render(): JSX.Element {
+    return (
+          <Html lang="pt-BR">
+            <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+              <link rel="shortcut icon" href="favicon.svg" />
+            </Head>
+            <body>
+              <Main />
+              <NextScript />
+            </body>
+          </Html>
+        )
+  }
 }
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,12 @@
+import "@fontsource/nunito";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  
+`;
