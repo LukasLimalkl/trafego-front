c4ce36cabd485d947c6f56c8fa5e637a7bca4446
LukasLimalkl <lukinhaslima063@gmail.com>
2024-02-24 20:32:06 -0300

    feat: adding globalStyles and Theme Provider to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 import Header from '@/components/header';
+import GlobalStyle from '@/styles/GlobalStyles';
+import { darkTheme, lightTheme } from '@/styles/theme/bodyModes/bodyModes';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Container } from './styles';
 
 export default function Home() {
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
   return (
-    <main>
-      <Header />
-    </main>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Container>
+        <Header />
+        <button onClick={themeToggler}>{theme}</button>
+      </Container>
+    </ThemeProvider>
   );
 }
