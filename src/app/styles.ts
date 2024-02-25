42f6d8889468b53581c1ebb4bb891c112b4f9be2
LukasLimalkl <lukinhaslima063@gmail.com>
2024-02-25 15:04:37 -0300

    feat: add color to background

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  .button-theme {
-    background: none;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  padding-top: 1.5rem;
+  .switch-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .sol {
     width: 40px;
     height: 40px;
+    color: ${(props) => props.theme.fontColor};
   }
+
+  .lua {
+    width: 30px;
+    height: 30px;
+    color: ${(props) => props.theme.fontColor};
+  }
+
   .span-switch {
     width: 50px;
   }
--- src/styles/theme/bodyModes/bodyModes.ts
+++ src/styles/theme/bodyModes/bodyModes.ts
@@ -1,9 +1,9 @@
 export const lightTheme = {
-  body: '#fff',
+  body: '#f3f2f5',
   fontColor: '#000'
 };
 
 export const darkTheme = {
-  body: '#000',
-  fontColor: '#fff'
+  body: 'linear-gradient(283deg, rgba(51,0,111,1) 0%, rgba(114,14,158,1) 56%);',
+  fontColor: '#f3f2f5'
 };
