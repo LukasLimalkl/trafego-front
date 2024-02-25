9f94dd754365fd44f07604e943c06093f050b63b
LukasLimalkl <lukinhaslima063@gmail.com>
2024-02-25 10:10:12 -0300

    feat: config icon and tittle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,19 @@
-'use client';
+import { Metadata } from 'next';
 import StyledComponentsRegistry from './lib/registry';
+
+export const metadata: Metadata = {
+  title: 'PaidFlow Control',
+  description: 'Website description',
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/favicon.icon',
+        href: '/favicon.pgn'
+      }
+    ]
+  }
+};
 
 export default function RootLayout({
   children
