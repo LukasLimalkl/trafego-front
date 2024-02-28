10d017b2629eb9a3a81271d67e03c2964af125cf
LukasLimalkl <lukinhaslima063@gmail.com>
2024-02-28 06:49:46 -0300

    feat: new color on theme

--- src/styles/theme/bodyModes/bodyModes.ts
+++ src/styles/theme/bodyModes/bodyModes.ts
@@ -1,9 +1,11 @@
 export const lightTheme = {
   body: '#f3f2f5',
-  fontColor: '#486BA1'
+  fontColor: '#486BA1',
+  buttonColor: '#4cbb17'
 };
 
 export const darkTheme = {
   body: '#486BA1',
-  fontColor: '#f3f2f5'
+  fontColor: '#f3f2f5',
+  buttonColor: '#37f713'
 };
