2cca2c0b188841ba0706f3ada32e7b1ce34e053b
LukasLimalkl <lukinhaslima063@gmail.com>
2024-02-27 21:14:19 -0300

    feat: adding buttonBox component

--- src/components/clientsForm/index.tsx
+++ src/components/clientsForm/index.tsx
@@ -1,22 +1,19 @@
+import ButtonBox from '../buttonBox';
 import { Container } from './styles';
 
 export default function ClientsForm() {
   return (
     <Container>
       <div className="box-clients">
-        <h3 className="title-clients">Clientes</h3>
+        <h3 className="title-clients">Clientes:</h3>
         <div className="search-container">
-          <input type="text" />
+          <input type="text" placeholder="Pesquisar" />
         </div>
         <form action="" className="all-clients">
           <ul className="ul-clients">
             <li className="list-clients">
               <span>Cliente</span>
-              <div className="button-box">
-                <button className="edit-button">Editar</button>
-                <button className="delete-button">Deletar</button>
-                <button className="verify-button">Verificar Anuncios</button>
-              </div>
+              <ButtonBox />
             </li>
           </ul>
         </form>
--- src/components/clientsForm/styles.ts
+++ src/components/clientsForm/styles.ts
@@ -8,13 +8,46 @@ export const Container = styled.div`
     flex-direction: column;
   }
 
+  input {
+    width: 350px;
+    background: #f3f2f5;
+    border: 2px solid ${(props) => props.theme.body};
+    border-radius: 10px;
+    height: 25px;
+    padding-left: 5px;
+  }
+
+  input:focus {
+    outline: 0;
+  }
+
+  .all-clients {
+    background-color: #f3f2f5;
+    width: 300px;
+    margin-top: 2rem;
+  }
+
   .title-clients {
     font-size: 1.6rem;
   }
 
-  .all-clients {
-  }
   li {
     list-style: none;
+  }
+
+  .list-clients {
+    display: flex;
+    flex-direction: row;
+    gap: 3rem;
+  }
+
+  .button-box {
+    display: flex;
+    flex-direction: column;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
   }
 `;
